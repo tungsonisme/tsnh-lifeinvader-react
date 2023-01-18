bf91d41e5592e3a74de355a0f0694a7d92711e27
Sean <tungson.nguyen@shopee.com>
2023-01-18 11:15:32 +0800

    wrap App with React

--- src/exposes/App.tsx
+++ src/exposes/App.tsx
@@ -1,10 +1,13 @@
+import React from 'react'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      Life Invader
-    </div>
+    <React.StrictMode>
+      <div className="App">
+        Life Invader
+      </div>
+    </React.StrictMode>
   )
 }
 
